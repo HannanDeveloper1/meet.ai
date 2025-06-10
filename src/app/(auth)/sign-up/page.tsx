80b1df9ba09b7dde15f9c0e5f7a65da9459b49b7
Hannan Dev <hannandeveloper1@gmail.com>
2025-06-10 20:26:04 +0500

    04: authentication ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { SignUpView } from "@/modules/auth/ui/sign-up-view";
+
+const SignUp = () => {
+  return <SignUpView />;
+};
+
+export default SignUp;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Tomorrow } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const tommorow = Tomorrow({
+  variable: "--font-tommorow",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${tommorow.variable} antialiased`}>
         {children}
       </body>
     </html>
